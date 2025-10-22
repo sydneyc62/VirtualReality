488318d3115eb470bec77f9bfc0b275b473d9582
sydneyc62 <sydneyc62@nycstudents.net>
2025-10-22 14:49:58 +0000

    boo ya

--- VRPractical2_2/script.js
+++ VRPractical2_2/script.js
@@ -5,20 +5,33 @@ window.addEventListener("DOMContentLoaded",function() {
     car = this.document.querySelector('#car');
     pokemonball = this.document.querySelector('#pokemonball');
     rocket = this.document.querySelector("#rocket");
+    dude = this.document.querySelector('#dude');
+    sun = this.document.querySelector('#sun')
 
     
     function loop(){
         
-        a+=0.1
+        a+=0.00001
         car.setAttribute("position",{x:-a,y:0,z:0});
         setTimeout(loop,10); 
 
         pokemonball.setAttribute("rotation",{x:a,y:0,z:0});
         setTimeout(loop,10); 
 
-        rocket.setAttribute("rotation",{x:a,y:0,z:0});
-        setTimeout(loop,10);         
+        rocket.setAttribute("position",{x:0,y:a,z:0});
+        setTimeout(loop,10); 
         
+        dude.setAttribute("scale",{x:a,y:a,z:a});
+        setTimeout(loop,10);
+        
+        sun.setAttribute("opacity",a);
+        setTimeout(loop,10);
+        
+        
+        
+
+
+
 
     }
 
