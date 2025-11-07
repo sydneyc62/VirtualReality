00d768c6ce2aefbff570b4024fb9401530038f73
sydneyc62 <sydneyc62@nycstudents.net>
2025-11-07 15:27:32 +0000

    am i the 2nd goat or ???

--- VRLesson2_5bClasswork/script.js
+++ VRLesson2_5bClasswork/script.js
@@ -1,5 +1,5 @@
 let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
-let scene, snowmen = [ ];
+let scene, snowmen = [ ],snowflakes=[],clouds=[];
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene"); //CSS Selector
@@ -19,9 +19,23 @@ window.addEventListener("DOMContentLoaded",function() {
   }
 
   //Challenge 1: Create an array to store 20 clouds in random positions
-  cloud = new Cloud(-5,10,-1);
+  for(let i = 0; i < 20; i++){
+    let x = rnd(-20,20);
+    let y = rnd(10,20);
+    let z = rnd(-20,20);
+    let cloud = new Cloud(x,y,z);
+    clouds.push(cloud);
+  }
 
   //Challenge 3: Create an array to store 100 snowflakes in random positions
+  for(let i = 0; i < 100; i++){
+    let x = rnd(0,20);
+    let y = rnd(0,20);
+    let z = rnd(0,20);
+    let snowflake = new Snowflake(x,y,z);
+    snowflakes.push(snowflake);
+  }
+
 
   loop();
 })
@@ -30,9 +44,10 @@ function loop(){
   for(let snowman of snowmen){
     snowman.spin();
   }
-  cloud.fly();
   //Challenge 2: Traverse your array of clouds and make each cloud fly
-
+  for(let cloud of clouds){
+    cloud.fly();
+  }
   //Challenge 4: Traverse your array of snowflakes and make each snowflake fall
   
   window.requestAnimationFrame( loop );
--- VRLesson2_7Classwork/dude.js
+++ VRLesson2_7Classwork/dude.js
@@ -3,17 +3,23 @@ class Dude{
     this.x = x;
     this.y = y;
     this.z = z;
+    this.dy=0.1;
     //Challenge 1: Add delta variables in order to make the dude walk forward on the z axis and jump up and down on the y axis.
-
+    this.dz = 0.1;
     this.obj = dudeTemplate.cloneNode(true);
     this.obj.setAttribute("position",{x:this.x,y:this.y,z:this.z});
     scene.append(this.obj);
   }
   jump(){
     //Challenge 2: Add the accumulators to modify the y and z variables
-
+    this.y += this.dy;
+    this.z += this.dz;
     //Challenge 3: Add the decision in order to alternate the delta on the y axis when the dude reaches less then 1 or greater than 2
-
+    if(this.y>1){
+      this.y+= this.dy;
+    }else if(this.y<2){
+      this.y-=this.dy;
+    };
     this.obj.setAttribute("position",{x:this.x,y:this.y,z:this.z});
   }
 }
