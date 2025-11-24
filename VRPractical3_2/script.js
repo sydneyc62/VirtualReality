bf31e37bd745074899497b5e74ebdf794694ea17
sydneyc62 <sydneyc62@nycstudents.net>
2025-11-24 15:36:59 +0000

    guh

--- VRPractical3_2/script.js
+++ VRPractical3_2/script.js
@@ -4,12 +4,23 @@ let scene;
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene")
 
-
+  car.getElementById("car");
+  car.x = 0;
+  car.dx = 0.05;
+  car.move = false;
+  car.addEventListener("click",function(){
+    car.move = true;
+  });
       
   loop();
 })
 
 function loop(){
+  if(car.move){
+    car.x += car.dx;
+    car.setAttribute("position",{x:car.dx,y:2,z:0});
+  }
+
 
     
   window.requestAnimationFrame(loop);
